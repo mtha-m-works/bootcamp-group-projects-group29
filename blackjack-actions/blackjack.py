c57d1a0ca6d669ba55f86010a0053328d07c30a9
Takalani Mapholi <takalanimapholi@icloud.com>
2026-09-18 13:48:15 +0200

    Ammendments made to def apply action

--- blackjack-actions/blackjack.py
+++ blackjack-actions/blackjack.py
@@ -58,4 +58,38 @@ def generate_actions(state):
 
 
 def apply_action(state, action, next_card=None):
+    def apply_action(state, action, next_card=None):
+    """
+    Apply action chosen by player.
+    """
+    hand = state["hand"]
+    # Check whether the action is allowed.
+    if action not in generate_actions(state):
+        raise ValueError(
+            f"'{action}' is not a legal action for this state"
+        )
+    #HIT
+    if action == "hit":
+        if next_card is None:
+            raise ValueError("'hit' requires a next_card")
+        return hand +[next_card]
+    #STAND
+    if action == "stand":
+        #The hand stays the same.
+        return hand
+    #DOUBLE
+    if action == "double":
+       if next_card is None:
+           raise ValueError("'double' requires a next_card")
+       # Add one card
+       return hand + [next_card]
+    #SURRENDER
+    if action == "surrender":
+        #Insurance does not change the hand.
+        return hand
+    #SPLIT
+    if action == "split":
+        card_a, card_b = hand
+        return[[card_a], [card_b]]
+    #If a new action is added but not implemented, this error will show.
     raise NotImplementedError("This function is not implemented yet.")
